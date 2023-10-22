881446036c6b8c6f6959e86b01e87efe9c37ee8e
Emirhan Tavus <Whisber1@gmail.com>
2023-10-22 10:28:16 +0300

    admin panel add

--- blogapp/blogAPI/admin.py
+++ blogapp/blogAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
+from .models import Comment
+from .models import User
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
